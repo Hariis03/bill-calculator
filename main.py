15b86a1e04d81e1c3cdb0eeb3928185ef4da661c
Hariis03 <86398484+Hariis03@users.noreply.github.com>
2022-02-10 04:10:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ split = input("How many people to split the bill? ")
 bill_to_number = float(bill) # Converts bill str input to float
 tipInt = bill_to_number * (int(tip) / 100) # adds the tip to the total bill cost
 splitInt = int(split) # converts split str input to int
-calculation = round((bill_to_number + tipInt) / splitInt, 2) #calculates the total cost with tip,divides to the split input,rounds to 2 numbers
+calculation = round((bill_to_number + tipInt) / splitInt, 2) #calculates the total cost with tip,splits the bill between x people,and rounds to 2 numbers after comma.
 
 print(f"Each person should pay: ${calculation}") # Prints the final cost per person.
